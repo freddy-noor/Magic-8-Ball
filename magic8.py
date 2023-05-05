35a0c8963e8ca7403acda535d9742c031756fe26
Freddy <79088826+freddy-noor@users.noreply.github.com>
2023-05-06 01:01:17 +0800

    Update magic8.py

--- magic8.py
+++ magic8.py
@@ -1,35 +1,31 @@
 import random
 
-name = "Freddy"
 question = "Will i become a millionaire?"
-answer = ""
 random_number = random.randint(1, 9)
-#print(random_number)
+
+print(question)
 
 if question == "":
   print("Please provide a question")
 elif random_number == 1:
-  answer = "Yes - definitely"
+ print("Magic 8-ball's answer: Yes - definitely")
 elif random_number == 2:
-  answer = "It is decidedly so"
+  print("Magic 8-ball's answer: It is decidedly so")
 elif random_number == 3:
-  answer = "Without a doubt"
+  print("Magic 8-ball's answer: Without a doubt")
 elif random_number == 4:
-  answer = "Reply hazy, try again"
+  print("Magic 8-ball's answer: Reply hazy, try again")
 elif random_number == 5:
-  answer = "Ask again later"
+  print("Magic 8-ball's answer: Ask again later")
 elif random_number == 6:
-  answer = "Better not tell you now"
+  print("Magic 8-ball's answer: Better not tell you now")
 elif random_number == 7:
-  answer = "My sources say no"
+  print("Magic 8-ball's answer: My sources say no")
 elif random_number == 8:
-  answer = "Outlook not so good"
+  print("Magic 8-ball's answer: Outlook not so good")
 elif random_number == 9:
-  answer = "Very doubtful"
+  print("Magic 8-ball's answer: Very doubtful")
 else:
   answer = "Error"
 
-#print(name + " asks: " + question)
-print(question)
-print("Magic 8-ball's answer: " + answer)
 
